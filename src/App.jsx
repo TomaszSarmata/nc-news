214a974a462fa586a885ad4d237501236e94a9ae
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-06-17 17:22:33 +0100

    implemented low fidelity ArticleDetails page to display info for a specific article

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route
-            path="/articles/:article_id"
+            path="/articles/:articleId"
             element={<ArticleDetails />}
           ></Route>
           <Route path="/add-article" element={<AddArticle />}></Route>
--- src/components/article-details/ArticleDetails.jsx
+++ src/components/article-details/ArticleDetails.jsx
@@ -1,3 +1,51 @@
+import { ncNewsApi } from "../../utils/api";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export function ArticleDetails() {
-  return <article>ArticleDetails</article>;
+  const [article, setArticle] = useState(null);
+  const { articleId } = useParams();
+
+  useEffect(() => {
+    ncNewsApi
+      .get(`/articles/${articleId}`)
+      .then(({ data }) => setArticle(data.article));
+  }, []);
+
+  const formatDate = (isoDateString) => {
+    const date = new Date(isoDateString);
+
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: "UTC",
+      timeZoneName: "short",
+    };
+
+    return date.toLocaleString("en-GB", options);
+  };
+
+  if (!article) {
+    return <div>loading...</div>;
+  }
+
+  const articleDate = formatDate(article.created_at);
+
+  return (
+    <article className="article-container">
+      <div className="article-image-container">
+        <img src={article.article_img_url} alt="" />
+      </div>
+      <div className="article-text-container">
+        <h3 className="article-title">Author: {article.title}</h3>
+        <p className="article-topic">Topic: {article.topic}</p>
+        <p className="article-author">Author: {article.author}</p>
+        <p className="article-date">Date: {articleDate}</p>
+      </div>
+    </article>
+  );
 }
