7e2b39d30e6db042ab6f72849c79719124c47678
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-06-18 20:01:17 +0100

    code clean up in ArticleDetails

--- src/components/article-details/ArticleDetails.jsx
+++ src/components/article-details/ArticleDetails.jsx
@@ -3,19 +3,15 @@ import { ncNewsApi } from "../../utils/api";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getArticleById } from "../../utils/api";
-// import { getComments } from "../../utils/api";
 import { formatDate } from "../../utils/helper";
-// import { submitComment } from "../../utils/api";
 import { CommentList } from "../comment-list/CommentList";
 
 export function ArticleDetails() {
   const [article, setArticle] = useState(null);
-  // const [comments, setComments] = useState(null);
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [votes, setVotes] = useState(null);
   const [hasVoted, setHasVoted] = useState(false);
-  // const [userComment, setUserComment] = useState("");
   const [loading, setLoading] = useState(false);
   const { articleId } = useParams();
 
@@ -25,12 +21,6 @@ export function ArticleDetails() {
       setVotes(article.votes);
     });
   }, []);
-
-  // useEffect(() => {
-  //   getComments(articleId).then(({ comments }) => {
-  //     setComments(comments);
-  //   });
-  // }, []);
 
   const handleUpVote = () => {
     if (hasVoted) return;
@@ -78,36 +68,9 @@ export function ArticleDetails() {
       });
   };
 
-  const handleUserComment = (e) => {
-    setUserComment(e.target.value);
-  };
-
-  const handleCommentSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    submitComment(articleId, {
-      username: "grumpy19",
-      body: userComment,
-      article_id: articleId,
-    })
-      .then(({ comment }) => {
-        setComments((currentComments) => {
-          return [comment, ...currentComments];
-        });
-        setLoading(false);
-        setUserComment("");
-      })
-
-      .catch((err) => console.log("error:", err));
-  };
-
   if (!article) {
     return <div>loading...</div>;
   }
-
-  // if (!comments) {
-  //   return <div>loading...</div>;
-  // }
 
   const articleDate = formatDate(article.created_at);
 
@@ -135,32 +98,6 @@ export function ArticleDetails() {
       </div>
 
       <CommentList articleId={articleId} />
-
-      {/* <div className="add-comment-container">
-        <h2>Add your comment</h2>
-        <p className={errorMessage ? "error" : ""}>{errorMessage}</p>
-        {loading ? <div>loading...</div> : null}
-        <form onSubmit={handleCommentSubmit}>
-          <label htmlFor="user-comment">Your Comment:</label>
-          <textarea
-            type="text"
-            id="user-comment"
-            onChange={handleUserComment}
-            value={userComment}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-      <ul className="article-comments-container">
-        <h2>Comments:</h2>
-        {comments.map((comment) => (
-          <li className="comment-card" key={comment.comment_id}>
-            <p className="comment-text">{comment.body}</p>
-            <p className="comment-author">{comment.author}</p>
-            <p className="comment-data">{formatDate(comment.created_at)}</p>
-          </li>
-        ))}
-      </ul> */}
     </article>
   );
 }
