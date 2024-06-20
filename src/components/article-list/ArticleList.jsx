873e90584e48208a678d2a5fe444376bd006b3a3
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-06-20 14:09:23 +0100

    implemented sorting and ordering data at the front of my application

--- src/components/article-list/ArticleList.jsx
+++ src/components/article-list/ArticleList.jsx
@@ -4,17 +4,14 @@ import { ncNewsApi } from "../../utils/api";
 import { useEffect, useState } from "react";
 import { getAllArticles } from "../../utils/api";
 import { Loader } from "../loader/Loader";
-import { useSearchParams } from "react-router-dom";
 
 export function ArticleList({ topic }) {
   const [articles, setArticles] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const sortBy = searchParams.get("sort_by") || "data";
-  const order = searchParams.get("order") || "desc";
+  const [order, setOrder] = useState("desc");
+  const [sortBy, setSortBy] = useState("date");
 
   useEffect(() => {
     setLoading(true);
@@ -37,37 +34,60 @@ export function ArticleList({ topic }) {
   }, [topic]);
 
   const handleSortChange = (e) => {
-    const newSortBy = e.target.value;
-    setSearchParams({ sort_by: newSortBy, order: order });
+    setSortBy(e.target.value);
   };
   const handleOrderChange = (e) => {
-    const newOrder = e.target.value;
-    setSearchParams({ sortBy: sortBy, order: newOrder });
+    setOrder(e.target.value);
   };
+
+  const sortedArticles = [...articles].sort((a, b) => {
+    if (sortBy === "date") {
+      if (order === "asc") {
+        return new Date(a.created_at) - new Date(b.created_at);
+      } else {
+        return new Date(b.created_at) - new Date(a.created_at);
+      }
+    } else if (sortBy === "comment") {
+      if (order === "asc") {
+        return a.comment_count - b.comment_count;
+      } else {
+        return b.comment_count - a.comment_count;
+      }
+    } else if (sortBy === "votes") {
+      if (order === "asc") {
+        return a.votes - b.votes;
+      } else {
+        return b.votes - a.votes;
+      }
+    }
+    return 0;
+  });
 
   if (loading) return <Loader />;
 
   return (
     <div className="article-list-wrapper">
-      <label>
-        Sort by:
-        <select value={sortBy} onChange={handleSortChange}>
-          <option value="date">Date</option>
-          <option value="comment">Comment Count</option>
-          <option value="votes">Votes</option>
-        </select>
-      </label>
-      <label>
-        Order:
-        <select value={order} onChange={handleOrderChange}>
-          <option value="desc">Descending</option>
-          <option value="asc">Ascending</option>
-        </select>
-      </label>
+      <div className="controllers-wrapper">
+        <label>
+          Sort by:
+          <select onChange={handleSortChange}>
+            <option value="date">Date</option>
+            <option value="comment">Comment Count</option>
+            <option value="votes">Votes</option>
+          </select>
+        </label>
+        <label>
+          Order:
+          <select onChange={handleOrderChange}>
+            <option value="desc">Descending</option>
+            <option value="asc">Ascending</option>
+          </select>
+        </label>
+      </div>
       <p className={errorMessage ? "error" : ""}>{errorMessage}</p>
       <p className={successMessage ? "success" : ""}>{successMessage}</p>
       <ul className="article-list">
-        {articles.map((article) => (
+        {sortedArticles.map((article) => (
           <ArticleCard key={article.article_id} article={article} />
         ))}
       </ul>
