85df7f03a1649232d9777ce0ceae02a97786c130
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-06-21 08:29:08 +0100

    created a list for the navbar and added some user photos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
-  return <section>Header</section>;
+  return (
+    <section className="header-wrapper">
+      <ul>
+        <li>
+          <Link to="/articles-by-topic/:topic">Search Articles</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link>Login</Link>
+        </li>
+      </ul>
+    </section>
+  );
 }
