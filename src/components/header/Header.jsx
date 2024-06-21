ae866e1777e1778bf9b191bbacb5b3c0e035650d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-06-21 14:19:50 +0100

    added more styling to the navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,27 +12,35 @@ export function Header() {
   };
   return (
     <section className="header-wrapper">
-      <ul className="nav-list">
-        <li className="nav-list-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-list-item grow">
-          <Link to="/articles-by-topic/:topic" className="nav-link">
-            Articles
-          </Link>
-        </li>
+      <div className="nav-container">
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/articles-by-topic/:topic" className="nav-link">
+              Articles
+            </Link>
+          </li>
 
-        <li className="nav-list-item" onClick={handleLogin}>
-          <Link to="/login" className="nav-link">
-            {user ? "Logout" : "Login"}
-          </Link>
-        </li>
-        <img src={user ? `/${user}.png` : `/loggedOut.png`} alt="" />
-      </ul>
+          <li className="nav-list-item" onClick={handleLogin}>
+            <Link to="/login" className="nav-link">
+              {user ? "Logout" : "Login"}
+            </Link>
+          </li>
+          <li className="nav-list-item">
+            <img
+              src={user ? `/${user}.png` : `/loggedOut.png`}
+              alt=""
+              className="user-icon"
+            />
+          </li>
+        </ul>
+      </div>
       <div className="header-img-container">
-        <img src="/banner.png" alt="" />
+        <img src="/banner.png" alt="Banner" />
       </div>
     </section>
   );
